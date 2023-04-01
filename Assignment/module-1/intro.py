17ce97fef55344ec517ec7dd25b71c26bb563851
Abhi8955 <120082500+Abhi8955@users.noreply.github.com>
2023-04-01 08:25:48 +0530

    Add files via upload

---
+++ Assignment/module-1/intro.py
@@ -0,0 +1,45 @@
+04.01 08:08
+What is Software?
+-> Software is a set of instructions and programs that tell a computer or other electronic device what to do. It includes computer programs, libraries, and related non-executable data, such as documentation and digital media. Software can be thought of as the operating system, applications, and other programs that are used to run a computer or other device. It can be created by software developers using programming languages and other tools, and can be distributed and installed by end-users to perform specific tasks or functions. Examples of software include word processing programs, web browsers, mobile apps, and operating systems such as Windows, macOS, and Linux.
+
+
+
+What are the types of Applications?
+->There are many types of applications, but some of the most common ones are:
+
+1.Desktop applications: These are applications that run on a computer's operating system and are typically installed on the computer's hard drive. Examples include productivity software, media players, and photo editors.
+
+2.Web applications: These are applications that run on a web server and are accessed through a web browser. Examples include social media platforms, e-commerce websites, and online banking.
+
+3.Mobile applications: These are applications that run on mobile devices such as smartphones and tablets. They can be downloaded and installed from app stores such as Apple App Store and Google Play Store. Examples include gaming apps, social media apps, and productivity apps.
+
+4.Cloud applications: These are applications that run on cloud servers and are accessed through a web browser or a specialized client. Examples include cloud storage services, collaboration tools, and software development platforms.
+
+5.Enterprise applications: These are applications that are designed for use within a large organization or enterprise. They may include enterprise resource planning (ERP) systems, customer relationship management (CRM) software, and human resource management (HRM) systems.
+
+6.Embedded applications: These are applications that are built into other devices or systems, such as cars, medical equipment, and home automation systems.
+
+7.Scientific applications: These are applications that are used in scientific research, such as data analysis software, simulation software, and visualization tools.
+
+
+
+ What is programing?
+->Programming is the process of creating software, applications, and computer programs using a programming language. It involves writing code in a specific syntax that can be executed by a computer or other electronic device.
+
+   Programming is a highly creative and logical process that involves solving problems and designing solutions using code. It typically involves breaking down a complex problem into smaller, more manageable parts and writing code to address each part.
+
+
+
+What is Python?
+->Python is an open-source, high-level programming language that is designed for readability and ease of use. It is dynamically typed, meaning that variable types are inferred at runtime rather than being explicitly declared in code.
+
+   Python emphasizes code readability and simplicity, with a clear, concise syntax that makes it easy to learn and write. It is an interpreted language, which means that code written in Python is executed directly by the computer without needing to be compiled first.
+
+    Python has a vast and active community of developers who contribute to its development and create libraries and frameworks that extend its capabilities. It is widely used in a variety of applications, including web development, scientific computing, data analysis, and machine learning, among others.
+
+
+
+
+
+
+
